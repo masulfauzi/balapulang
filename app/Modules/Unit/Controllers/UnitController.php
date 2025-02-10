6332432dfd271a3b4bfa955aabdb295f2d186bda
masulfauzi <masul.fauzi@gmail.com>
2025-02-10 16:01:35 +0700

    fix: ubah tampilan index unit

--- app/Modules/Unit/Controllers/UnitController.php
+++ app/Modules/Unit/Controllers/UnitController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Modules\Unit\Controllers;
 
 use Form;
@@ -16,7 +17,7 @@ class UnitController extends Controller
 	use Logger;
 	protected $log;
 	protected $title = "Unit";
-	
+
 	public function __construct(Log $log)
 	{
 		$this->log = $log;
@@ -25,27 +26,28 @@ class UnitController extends Controller
 	public function index(Request $request)
 	{
 		$query = Unit::query();
-		if($request->has('search')){
+		if ($request->has('search')) {
 			$search = $request->get('search');
 			// $query->where('name', 'like', "%$search%");
 		}
 		$data['data'] = $query->paginate(10)->withQueryString();
 
-		$this->log($request, 'melihat halaman manajemen data '.$this->title);
+		$this->log($request, 'melihat halaman manajemen data ' . $this->title);
 		return view('Unit::unit', array_merge($data, ['title' => $this->title]));
 	}
 
 	public function create(Request $request)
 	{
-		$ref_dinas = Dinas::all()->pluck('nama_dinas','id');
-		
+		$ref_dinas = Dinas::all()->pluck('nama_dinas', 'id');
+		$ref_dinas->prepend('-PILIH SALAH SATU-', '');
+
 		$data['forms'] = array(
-			'id_dinas' => ['Dinas', Form::select("id_dinas", $ref_dinas, null, ["class" => "form-control select2"]) ],
-			'nama_unit' => ['Nama Unit', Form::text("nama_unit", old("nama_unit"), ["class" => "form-control","placeholder" => "", "required" => "required"]) ],
-			
+			'id_dinas' => ['Dinas', Form::select("id_dinas", $ref_dinas, null, ["class" => "form-control select2"])],
+			'nama_unit' => ['Nama Unit', Form::text("nama_unit", old("nama_unit"), ["class" => "form-control", "placeholder" => "", "required" => "required"])],
+
 		);
 
-		$this->log($request, 'membuka form tambah '.$this->title);
+		$this->log($request, 'membuka form tambah ' . $this->title);
 		return view('Unit::unit_create', array_merge($data, ['title' => $this->title]));
 	}
 
@@ -54,17 +56,17 @@ class UnitController extends Controller
 		$this->validate($request, [
 			'id_dinas' => 'required',
 			'nama_unit' => 'required',
-			
+
 		]);
 
 		$unit = new Unit();
 		$unit->id_dinas = $request->input("id_dinas");
 		$unit->nama_unit = $request->input("nama_unit");
-		
+
 		$unit->created_by = Auth::id();
 		$unit->save();
 
-		$text = 'membuat '.$this->title; //' baru '.$unit->what;
+		$text = 'membuat ' . $this->title; //' baru '.$unit->what;
 		$this->log($request, $text, ['unit.id' => $unit->id]);
 		return redirect()->route('unit.index')->with('message_success', 'Unit berhasil ditambahkan!');
 	}
@@ -73,7 +75,7 @@ class UnitController extends Controller
 	{
 		$data['unit'] = $unit;
 
-		$text = 'melihat detail '.$this->title;//.' '.$unit->what;
+		$text = 'melihat detail ' . $this->title; //.' '.$unit->what;
 		$this->log($request, $text, ['unit.id' => $unit->id]);
 		return view('Unit::unit_detail', array_merge($data, ['title' => $this->title]));
 	}
@@ -82,15 +84,15 @@ class UnitController extends Controller
 	{
 		$data['unit'] = $unit;
 
-		$ref_dinas = Dinas::all()->pluck('nama_dinas','id');
-		
+		$ref_dinas = Dinas::all()->pluck('nama_dinas', 'id');
+
 		$data['forms'] = array(
-			'id_dinas' => ['Dinas', Form::select("id_dinas", $ref_dinas, null, ["class" => "form-control select2"]) ],
-			'nama_unit' => ['Nama Unit', Form::text("nama_unit", $unit->nama_unit, ["class" => "form-control","placeholder" => "", "required" => "required", "id" => "nama_unit"]) ],
-			
+			'id_dinas' => ['Dinas', Form::select("id_dinas", $ref_dinas, null, ["class" => "form-control select2"])],
+			'nama_unit' => ['Nama Unit', Form::text("nama_unit", $unit->nama_unit, ["class" => "form-control", "placeholder" => "", "required" => "required", "id" => "nama_unit"])],
+
 		);
 
-		$text = 'membuka form edit '.$this->title;//.' '.$unit->what;
+		$text = 'membuka form edit ' . $this->title; //.' '.$unit->what;
 		$this->log($request, $text, ['unit.id' => $unit->id]);
 		return view('Unit::unit_update', array_merge($data, ['title' => $this->title]));
 	}
@@ -100,18 +102,18 @@ class UnitController extends Controller
 		$this->validate($request, [
 			'id_dinas' => 'required',
 			'nama_unit' => 'required',
-			
+
 		]);
-		
+
 		$unit = Unit::find($id);
 		$unit->id_dinas = $request->input("id_dinas");
 		$unit->nama_unit = $request->input("nama_unit");
-		
+
 		$unit->updated_by = Auth::id();
 		$unit->save();
 
 
-		$text = 'mengedit '.$this->title;//.' '.$unit->what;
+		$text = 'mengedit ' . $this->title; //.' '.$unit->what;
 		$this->log($request, $text, ['unit.id' => $unit->id]);
 		return redirect()->route('unit.index')->with('message_success', 'Unit berhasil diubah!');
 	}
@@ -123,9 +125,8 @@ class UnitController extends Controller
 		$unit->save();
 		$unit->delete();
 
-		$text = 'menghapus '.$this->title;//.' '.$unit->what;
+		$text = 'menghapus ' . $this->title; //.' '.$unit->what;
 		$this->log($request, $text, ['unit.id' => $unit->id]);
 		return back()->with('message_success', 'Unit berhasil dihapus!');
 	}
-
 }
